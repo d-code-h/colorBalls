19c7409b54d7d861cc95f99cfa7257b0f8264ebd
Habeeb David <david@localhost.localdomain>
2020-11-11 12:11:46 +0100

    fix bug in app file

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// Packages
-const   bodyParser              =   require("body-parser"),
-        express                 =   require("express"),
+const   express                 =   require("express"),
         app                     =   express(),
         mongoose                =   require("mongoose"),
         passport                =   require("passport"),
         LocalStrategy           =   require("passport-local"),
         passportLocalMongoose   =   require("passport-local-mongoose"),
-        flash                   =   require("connect-flash");
+        flash                   =   require("connect-flash"),
+        createError             =   require('http-errors'),
+        path                    =   require('path'),
+        cookieParser            =   require('cookie-parser'),
+        logger                  =   require('morgan'),
+        indexRouter             =   require('./routes/index'); //index Route
+
 
 // Configurations
 // mongoose
@@ -29,10 +33,15 @@ userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
-// Others
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 
 // passport
@@ -49,80 +58,22 @@ app.use(function(req, res, next){
     next();
 });
 
+app.use('/', indexRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// ROUTES
-// index
-app.get("/", (req, res) => {
-    res.redirect("/login");
-});
-// login
-app.get("/login", (req, res) => {
-    res.render("login");
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// login logic
-app.post("/login", passport.authenticate("local", {
-    // successRedirect: "/game",
-    failureRedirect: "/login",
-    failureFlash: true
-}), (req, res, next) => {
-    // issue a remember me cookie if the option was checked
-    if (!req.body.remember_me) { 
-      req.session.cookie.expires=false;
-    next(); 
-    }else {
-        req.session.cookie.originalMaxAge = 604800000; 
-        next();
-    }
-},
-function(req, res) {
-  res.redirect('/game');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-// register
-app.get("/register", (req, res) => {
-    res.render("register");
-});
-// register logic
-app.post("/register", (req, res) => {
-    if (req.body.password == req.body.confPassword){
-        User.register(new User({username: req.body.username}), req.body.password, (err, createdUser) => {
-            if (err){
-                req.flash("error", "Something went wrong");
-                res.redirect("/register");
-            }else {
-                passport.authenticate("local")(req, res, () => {
-                    req.flash("success", "Registration was successful, Enjoy your stay");
-                    res.redirect("/game");
-                });
-            }
-        });
-    }else {
-        req.flash("error", "Your passwords do not correlate");
-        res.redirect("back");
-    }
-});
-// log out
-app.get("/logout", (req, res) => {
-    req.logout();
-    req.flash("success", "You logged out");
-    res.redirect("/login");
-});
-// game
-app.get("/game", isLoggedIn, (req, res) => {
-    res.render("index", {user: req.user});
-})
 
-// isLoggedIn
-function isLoggedIn(req, res, next){
-    if (req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "Please log in");
-    res.redirect("/login");
-}
-
-// Server
-app.listen(3001, () => {
-    console.log("Server started at localhost: 3001" );
-});
+module.exports = app;
